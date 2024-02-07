fcb8c03dd4f63a0d3c6d3ba0c2796b2db9acb862
Maizier Barid <maizier.b@gmail.com>
2024-02-07 21:25:03 +0700

    update judul menu pengguna

--- view/m_user/index.php
+++ view/m_user/index.php
@@ -61,13 +61,13 @@ include '../footer.php';
 			<div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-					<div class="breadcrumb-title pe-3">Tables</div>
+					<div class="breadcrumb-title pe-3">Admin</div>
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">Data Table</li>
+								<li class="breadcrumb-item active" aria-current="page">Data Pengguna</li>
 							</ol>
 						</nav>
 					</div>
